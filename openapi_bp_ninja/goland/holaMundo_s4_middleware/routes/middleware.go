c1f983fd8e2df544a6910fb9cb7c7e5a33b0db31
Carmen J. Cabezas <carmen.jimenez.cabezas@bbva.com>
2020-01-17 13:03:05 +0100

    updated background color of pre

--- openapi_bp_ninja/goland/holaMundo_s4_middleware/routes/middleware.go
+++ openapi_bp_ninja/goland/holaMundo_s4_middleware/routes/middleware.go
@@ -12,9 +12,8 @@ const clientIsTheBossCtx = "isTheBoss"
 
 func (s *HolaMundo) CheckBossCert(res http.ResponseWriter, req *http.Request) (context.Context, error) {
 	ctx := context2.New(req.Context())
-	isABoss := false
-	if cn := ctx.ClientCert().Subject.CommonName; strings.ToLower(cn) == cnBoss {
-		isABoss = true
-	}
+
+	isABoss := strings.ToLower(ctx.ClientCert().Subject.CommonName) == cnBoss
+
 	return ctx.SetValue(clientIsTheBossCtx, isABoss).Context(), nil
 }
